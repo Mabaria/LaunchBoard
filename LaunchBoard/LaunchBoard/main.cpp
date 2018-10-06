26a0d385de902593a10a7d1dc7ede51ebffa125b
Mabaria <magnomrikard@gmail.com>
2018-10-06 13:44:33 +0200

    Hello World

---
+++ LaunchBoard/LaunchBoard/main.cpp
@@ -0,0 +1,13 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	std::getchar();
+    return 0;
+}
+
